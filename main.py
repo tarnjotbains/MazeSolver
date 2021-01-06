0764cbfe521001d1fb8ef8d3550500500e7d3019
tarnjotbains <tarnjotbains@gmail.com>
2021-01-06 12:30:10 -0500

    Everything is now fully functional.
    Todo:
    Still have to clean up the code and add docstrings.
    
    Bugs:
    When mouse starts on screen, the program does not work correctly sometimes.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame 
 from pygame.locals import * 
 import random 
+from queue import PriorityQueue
+from model import MazeNode, Line 
 
 #initialize pygame
 pygame.init() 
@@ -16,83 +18,6 @@ screen = pygame.display.set_mode((800,800))
 #load images
 seekerImg = pygame.image.load('seeker.png')
 itemImg = pygame.image.load('item.png')
-
-
-class Line(): 
-    def __init__(self, start_pos, end_pos):
-        self.start_pos = start_pos
-        self.end_pos = end_pos
-
-
-class MazeNode: 
-    def __init__(self, data): 
-        self.data = data
-        self.visited = False
-        
-        self.left_neighbor= None
-        self.right_neighbor = None
-        self.top_neighbor = None
-        self.bottom_neighbor = None 
-        self.neighbor_list = [] 
-
-        self.border_left = None
-        self.border_right = None
-        self.border_top = None
-        self.border_bottom = None
-        self.border_list = []
-
-        #For Djikstras
-
-        self.root = None 
-        self.weight = 1
-    
-    def generate_borders(self): 
-        if self.data[0] != 0: 
-            start_pos = (self.data[0]*40, self.data[1]*40)
-            end_pos = (self.data[0]*40, self.data[1]*40 + 40)
-            self.border_left = Line(start_pos, end_pos)
-            self.border_list.append(self.border_left) 
-
-        if self.data[0] != 19: 
-            start_pos = (self.data[0]*40 + 40, self.data[1]*40)
-            end_pos = (self.data[0]*40 + 40, self.data[1]*40 + 40)
-            self.border_right = Line(start_pos, end_pos)
-            self.border_list.append(self.border_right) 
-
-        if self.data[1] != 0: 
-            start_pos = (self.data[0]*40, self.data[1]*40)
-            end_pos = (self.data[0]*40 + 40 , self.data[1]*40)
-            self.border_top = Line(start_pos, end_pos)
-            self.border_list.append(self.border_top) 
-
-        if self.data[1] != 19: 
-            start_pos = (self.data[0]*40, self.data[1]*40 +40)
-            end_pos = (self.data[0]*40 + 40, self.data[1]*40 + 40)
-            self.border_bottom = Line(start_pos, end_pos)
-            self.border_list.append(self.border_bottom) 
-    
-    def remove_border(self, neighbor): 
-
-
-        if neighbor.data[0] == (self.data[0] + 1): 
-            self.border_list.remove(self.border_right)
-            self.border_right = None 
-        if neighbor.data[0] == (self.data[0] - 1): 
-            self.border_list.remove(self.border_left)
-            self.border_left = None
-        if neighbor.data[1] == (self.data[1] - 1): 
-            self.border_list.remove(self.border_top) 
-            self.border_top = None
-        if neighbor.data[1] == (self.data[1] + 1): 
-            self.border_list.remove(self.border_bottom) 
-            self.border_bottom = None
-
-    def draw_border(self): 
-        for line in self.border_list: 
-            pygame.draw.line(screen, (255,0,0), line.start_pos, line.end_pos) 
-
-
-
 
 
 def generate_maze_nodes(): 
@@ -123,51 +48,66 @@ def generate_neighbors(node_list):
 
 def carve(node, node_list, screen): 
     node.visited = True 
-
-
     random.shuffle(node.neighbor_list) 
+    
+    #visuals
     screen.fill((0,0,0))
     draw_borders(node_list, screen)
     pygame.display.update() 
+
     for neighbor in node.neighbor_list: 
         if neighbor.visited == False: 
+
+            #apply visuals        
             node.remove_border(neighbor) 
             neighbor.remove_border(node)  
             pygame.display.flip() 
+
+            #recurse
             carve(neighbor, node_list, screen) 
 
     
     return 
 
+def update_neighbors(node_list): 
+    for node in node_list:
+        if node.border_left and node.left_neighbor:
+            node.neighbor_list.remove(node.left_neighbor) 
+            node.left_neighbor = None 
+        if node.border_right and node.right_neighbor: 
+            node.neighbor_list.remove(node.right_neighbor)
+            node.right_neighbor = None
+        if node.border_top and node.top_neighbor: 
+            node.neighbor_list.remove(node.top_neighbor) 
+            node.top_neighbor = None 
+        if node.border_bottom and node.bottom_neighbor: 
+            node.neighbor_list.remove(node.bottom_neighbor) 
+            node.bottom_neighbor = None 
+                        
 
-#Djikstras: Still working on this.... 
-"""def search(node, node_goal): 
-
+def search(node, queue, node_goal): 
     if node == node_goal: 
-        #do something 
+        return node 
+    if node_goal.visited == True: 
+        return node_goal 
+    if node.visited == True: 
+        return 
+
+    node.visited = True 
+    for neighbor in node.neighbor_list:
+        if not neighbor.visited: 
+            if neighbor.weight != -1: 
+                if neighbor.weight > node.weight + 1:
+                    neighbor.root = node
+                    queue.put((node.weight + 1, neighbor))
+                    neighbor.weight  =  node.weight + 1
+            elif neighbor.weight == -1: 
+                neighbor.root = node 
+                queue.put((node.weight + 1, neighbor))
+                neighbor.weight = node.weight + 1 
     
-    unvisited_neighbors = [] 
-    min_distance = 0 
-    for neighbor in node.neighbor_list: 
-        if neighbor.visited == False: 
-            if neighbor.weight > neighbor.weight + node.distance
-                neighbor.weight += node.distance 
-                if min_distance == 0 or neighbor.weight < min_distance:
-                    min_distance = neighbor.weight 
-
-            unvisited_neighbors.append(neighbor) 
-    
-    for unvisited_neighbor in unvisited_neighbors: 
-        if unvisited_neighbor.distance == min_distance: 
-            node.visited = True 
-            search(unvisited_neighbor)""" 
-
-
-    
-
-
-
-
+    node_to_visit = queue.get()[1] 
+    search(node_to_visit, queue, node_goal) 
 
 def reset_visited(node_list): 
     for node in node_list: 
@@ -187,44 +127,60 @@ def draw_prompt(prompt):
 
 node_list = generate_maze_nodes() 
 generate_neighbors(node_list) 
-pygame.display.update() 
+
+
 
 def run_maze(): 
-    global screen 
-    running = True 
+    global screen
 
+    node_list = generate_maze_nodes() 
+    generate_neighbors(node_list)
 
-    global node_list 
-    
-    
-
+    # Boolean values used to update the view at certain times. 
     maze_carved = False
     seeker_in_position = False 
     item_in_position = False 
+    path_generated = False 
+    path_drawn = False 
+    running = True 
 
     while running: 
+
+        # Make the background black.
         screen.fill((0,0,0))
 
-        draw_borders(node_list, screen) 
+        # Draw borders around cells 
+        draw_borders(node_list, screen)
+
+        # Create the maze. 
         if maze_carved == False: 
             carve(node_list[45], node_list, screen)  
             maze_carved = True 
             pygame.display.update()
             maze_carved = True  
+            # Reset state of all nodes to unvisited for Dijkstra's. 
+            reset_visited(node_list) 
+            update_neighbors(node_list) 
 
-        # Reset state of all nodes to unvisited so we can use djikstras. 
-        reset_visited(node_list) 
-
+        # Handle Events 
         for event in pygame.event.get(): 
+
+            # If user closes the program. 
             if event.type == QUIT: 
                 exit()
+
+            # If user places the item.     
             if event.type == MOUSEBUTTONDOWN and seeker_in_position and not item_in_position:
                 mx, my = pygame.mouse.get_pos() 
                 mx , my = mx//40 , my//40
                 itemX , itemY = (mx*40+10, my*40+10)
                 item_in_position = True 
 
+                for maze_node in node_list: 
+                    if maze_node.data == (mx,my):
+                        item_node = maze_node 
 
+            # If user places the seeker. 
             if event.type == MOUSEBUTTONDOWN and maze_carved and not seeker_in_position: 
                 mx, my = pygame.mouse.get_pos() 
                 mx , my = mx//40 , my//40
@@ -235,8 +191,7 @@ def run_maze():
                 for maze_node in node_list: 
                     if maze_node.data == (mx,my):
                         seeker_node = maze_node
-                        seeker_node.weight = 0               
-
+                        seeker_node.weight = 0    
 
         # Player prompt to place seeker. 
         if maze_carved == True and not seeker_in_position: 
@@ -260,14 +215,30 @@ def run_maze():
         if item_in_position:
             screen.blit(itemImg, (itemX, itemY))
 
+        # Run Djikstras 
+        if seeker_in_position and item_in_position:
+            q = PriorityQueue() 
+            search(seeker_node,q, item_node) 
+            path_generated = True 
+        
+        # Draw path from seeker to item (frame by frame). 
+        if path_generated and not path_drawn: 
+            current_node = item_node.root
+            while current_node != seeker_node: 
+                current_node.draw(screen, (255,0,127)) 
+                current_node = current_node.root 
+                pygame.display.update() 
+            path_drawn = True 
+
+        # Static path from seeker to item (if the initial path is drawn).
+        if path_drawn: 
+            current_node = item_node.root
+            while current_node != seeker_node: 
+                current_node.draw(screen, (255,0,127)) 
+                current_node = current_node.root 
+        
+        # Update the view. 
         pygame.display.update() 
-
-        
-
-    
-        
-
-    
 
 
 
--- model.py
+++ model.py
@@ -1,61 +1,84 @@
+import pygame 
+from pygame.locals import * 
 import random 
 
-class graph: 
-    def __init__(self, graph_dict):
-        self.graph_dict = graph_dict
-
-    def verticies(self):
-        return list(self.graph_dict.keys()) 
-    
-    def edges(self): 
-        return self.generate_edges()
-    
-    def add_vertex(self, vertex): 
-        if vertex not in self.verticies(): 
-            self.graph_dict[vertex] = [] 
-
-    def add_edge(self, edge): 
-        vertex1, vertex2 = edge 
-
-        if vertex1 not in self.verticies():
-            self.graph_dict[vertex1].append(vertex2) 
-        elif vertex1 in self.verticies(): 
-            self.graph_dict[vertex1].append(vertex2)
+class Line(): 
+    def __init__(self, start_pos, end_pos):
+        self.start_pos = start_pos
+        self.end_pos = end_pos
 
 
-    def generate_edges(self): 
-        edges = [] 
-        for vertex in self.graph_dict: 
-            for neighbour in self.graph_dict[vertex]: 
-                edges.append((vertex, neighbour)) 
-        return edges 
-
-
-class Node: 
-    def __init__(self,data): 
-        
-        self.left= [None, False]
-        self.right = [None, False]
-        self.top = [None, False]
-        self.bottom = [None, False]
-
+class MazeNode: 
+    def __init__(self, data): 
         self.data = data
-    
-    def is_neighbour(self, node2): 
-
-        if self.data[1] == node2.data[1]: 
-            if self.data[0] == node2.data[0] -1 or self.data[0] == node2.data[0] + 1: 
-                return True 
-
-        if self.data[0] == node2.data[0]: 
-            if self.data[1] == node2.data[1] -1 or self.data[1] == node2.data[1] + 1: 
-                return True
-
-        return False 
+        self.visited = False
         
+        self.left_neighbor= None
+        self.right_neighbor = None
+        self.top_neighbor = None
+        self.bottom_neighbor = None 
+        self.neighbor_list = [] 
+
+        self.border_left = None
+        self.border_right = None
+        self.border_top = None
+        self.border_bottom = None
+        self.border_list = []
+
+        #For Djikstras
+        self.root = None 
+        self.weight = -1
+    
+    def generate_borders(self): 
+        if self.data[0] != 0: 
+            start_pos = (self.data[0]*40, self.data[1]*40)
+            end_pos = (self.data[0]*40, self.data[1]*40 + 40)
+            self.border_left = Line(start_pos, end_pos)
+            self.border_list.append(self.border_left) 
+
+        if self.data[0] != 19: 
+            start_pos = (self.data[0]*40 + 40, self.data[1]*40)
+            end_pos = (self.data[0]*40 + 40, self.data[1]*40 + 40)
+            self.border_right = Line(start_pos, end_pos)
+            self.border_list.append(self.border_right) 
+
+        if self.data[1] != 0: 
+            start_pos = (self.data[0]*40, self.data[1]*40)
+            end_pos = (self.data[0]*40 + 40 , self.data[1]*40)
+            self.border_top = Line(start_pos, end_pos)
+            self.border_list.append(self.border_top) 
+
+        if self.data[1] != 19: 
+            start_pos = (self.data[0]*40, self.data[1]*40 +40)
+            end_pos = (self.data[0]*40 + 40, self.data[1]*40 + 40)
+            self.border_bottom = Line(start_pos, end_pos)
+            self.border_list.append(self.border_bottom) 
+    
+    def remove_border(self, neighbor): 
+        if neighbor.data[0] == (self.data[0] + 1): 
+            self.border_list.remove(self.border_right)
+            self.border_right = None 
+        if neighbor.data[0] == (self.data[0] - 1): 
+            self.border_list.remove(self.border_left)
+            self.border_left = None
+        if neighbor.data[1] == (self.data[1] - 1): 
+            self.border_list.remove(self.border_top) 
+            self.border_top = None
+        if neighbor.data[1] == (self.data[1] + 1): 
+            self.border_list.remove(self.border_bottom) 
+            self.border_bottom = None
+
+    def draw_border(self, screen): 
+        for line in self.border_list: 
+            pygame.draw.line(screen, (255,0,0), line.start_pos, line.end_pos)
+
+    def draw(self, screen, color): 
+        
+        pygame.draw.rect(screen,color, pygame.Rect(self.data[0]*40+15, self.data[1]*40 + 15, 10,10) ) 
 
 
-
+    def __lt__(self,other): 
+        return self.weight < other.weight
 
 
 
